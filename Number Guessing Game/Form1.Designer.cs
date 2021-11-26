5c555b37f42c52042aee64775026ac01e754e5b5
MOOICT <MOOICT@DESKTOP-OQHBRE0>
2021-11-26 21:49:39 +0000

    Add project files.

---
+++ Number Guessing Game/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Number_Guessing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtEnterNumber = new System.Windows.Forms.TextBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(316, 260);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(141, 62);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.CheckNumber);
+            // 
+            // txtEnterNumber
+            // 
+            this.txtEnterNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnterNumber.Location = new System.Drawing.Point(208, 196);
+            this.txtEnterNumber.Name = "txtEnterNumber";
+            this.txtEnterNumber.Size = new System.Drawing.Size(359, 31);
+            this.txtEnterNumber.TabIndex = 1;
+            this.txtEnterNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(90, 74);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(477, 31);
+            this.lblQuestion.TabIndex = 2;
+            this.lblQuestion.Text = "I am thinking of a number between: ";
+            // 
+            // lblGuessed
+            // 
+            this.lblGuessed.AutoSize = true;
+            this.lblGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessed.Location = new System.Drawing.Point(204, 380);
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Size = new System.Drawing.Size(177, 20);
+            this.lblGuessed.TabIndex = 3;
+            this.lblGuessed.Text = "You guessed 0 times";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.txtEnterNumber);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Number Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtEnterNumber;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblGuessed;
+    }
+}
+
---
+++ Number Guessing Game/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing_Game
+{
+    public partial class Form1 : Form
+    {
+
+        Random randomNumber = new Random();
+
+        int number = 0;
+        int guesses = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadQuestions();
+        }
+
+        private void CheckNumber(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(txtEnterNumber.Text);
+
+            guesses += 1;
+            lblGuessed.Text = "You guessed " + guesses + " times";
+
+            if (i == number)
+            {
+                MessageBox.Show("Nice, you guessed it. Try another");
+                loadQuestions();
+                txtEnterNumber.Text = "";
+                guesses = 0;
+                lblGuessed.Text = "You guessed " + guesses + " times";
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Go Higher");
+            }
+            else
+            {
+                MessageBox.Show("Go Lower");
+            }
+
+
+        }
+
+        private void loadQuestions()
+        {
+            number = randomNumber.Next(0, 10);
+
+            lblQuestion.Text = "I am thinking of a number between: 0 and 10 ";
+        }
+    }
+}
